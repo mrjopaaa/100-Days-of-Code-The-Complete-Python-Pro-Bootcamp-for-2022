bc6b80873f2d9e87aba7d84a149c7219ba42db7d
mrjopaaa <jocotic@gmail.com>
2022-10-04 23:24:05 +0200

    "Day 16" part 3

--- day16-Coffee-Machine-OOP/main.py
+++ day16-Coffee-Machine-OOP/main.py
@@ -32,38 +32,27 @@
 # create objects from blueprint classes:
 # e.g: car = CarBluePrint()
 
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-
 is_off = False
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+menu = Menu()
 
 while not is_off:
-    user_input = input(str("What would you like? (espresso/latte/cappuccino): "))
-    if user_input == "off":
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ")
+    if choice == "off":
         print("Turning off machine...")
         is_off = True
-    if user_input == "report":
-        coffee_maker = CoffeeMaker()
-        print(coffee_maker.report())
-        money_machine = MoneyMachine()
+    elif choice == "report":
+        coffee_maker.report()
         money_machine.report()
-        # print(f"Money: ${MoneyMachine.report()}")
     else:
-        menu = Menu()
-        process_payment = MoneyMachine()
-        make_coffee = CoffeeMaker()
-        if menu.find_drink(order_name=user_input):
-            if process_payment.make_payment(menu.find_drink(order_name=user_input).cost):
-                if make_coffee.is_resource_sufficient(menu.find_drink(order_name=user_input)):
-                    make_coffee.make_coffee(menu.find_drink(order_name=user_input))
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            money_machine.make_payment(drink.cost)
+            coffee_maker.make_coffee(drink)
 
-    #         if money_for_calculation >= drink_ingredients["cost"]:
-    #             change_money = round(money_for_calculation - drink_ingredients["cost"],2)
-    #             make_coffee(user_input, drink_ingredients["ingredients"])
-    #             print(f"Here is ${change_money} dollars in change")
-    #             money_for_calculation = round(money_for_calculation - change_money, 2)
-    #             add_profit(money_for_calculation)
-    #         else:
-    #             print("Sorry that's not enough money. Money refunded.")
